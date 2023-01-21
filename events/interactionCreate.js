5c41fe029445b0b40dd2ecaf881f8888310bc5ee
IndifferentSoul <whitecloudlurker@gmail.com>
2023-01-21 20:29:10 +0200

    Moved autocomplete to command file

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,4 @@
 const { InteractionType } = require("discord.js");
-const { worldNames } = require('../utils/editedworldnames');
 
 module.exports = {
 	name: 'interactionCreate',
@@ -17,31 +16,17 @@ module.exports = {
 			}
 		}
 
-		if (interaction.type === InteractionType.ApplicationCommandAutocomplete && interaction.commandName === "wvwinfo") {
+		if (interaction.isAutocomplete()) {
+			const slashcommand = interaction.client.slashcommands.get(interaction.commandName);
+
+			if (!slashcommand) return;
+
 			try {
-				const focusedValue = interaction.options.getFocused();
-				if (focusedValue === '') {  		//If nothing is written return no search results
-					await interaction.respond('')
-				} else {
-					const choices = worldNames;
-					const filtered = choices.filter(choice => choice.toLowerCase().includes(focusedValue.toLowerCase()));
-
-					if (filtered.length >= 25) {		//Can't display more than 25 results with autocomplete
-						const sliced = filtered.slice(0, 25);
-						await interaction.respond(
-							sliced.map(choice => ({ name: choice, value: choice })),
-						);
-					} else {
-						await interaction.respond(
-							filtered.map(choice => ({ name: choice, value: choice })),
-						);
-					};
-				}
-
+				await slashcommand.autocomplete(interaction);
 			} catch (error) {
-				console.log(error)
-			}
-		}
+				console.log(error);
+			};
+		};
 
 	},
 };
